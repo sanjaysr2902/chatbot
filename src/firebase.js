95fea51294bbd5f75d889a7d5d6d72f94f5c67ed
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-07 17:15:38 +0530

    now

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyC_jME336yyYJND2AXSM93kMIQCvuQ9Pq0",
+  authDomain: "sachusaaru.firebaseapp.com",
+  projectId: "sachusaaru",
+  storageBucket: "sachusaaru.firebasestorage.app",
+  messagingSenderId: "33589168724",
+  appId: "1:33589168724:web:44746dbc99a68e3e773bc1",
+  measurementId: "G-K637RDPCHC"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
